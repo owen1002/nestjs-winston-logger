02001036505f4b25aba4b9e3b5e90b2a7c2f4133
Owen Siu <owensiu1002@gmail.com>
2021-01-13 16:28:49 +0800

    fix import

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export * from "src/nest-winston-logger.constants";
+export * from "src/nest-winston-logger.interceptor";
+export * from "src/nest-winston-logger.module";
+export * from "src/nest-winston-logger.service";
