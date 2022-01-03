cc56614fbfd79b4b9e55b25c1339ebb646aaea98
Owen Siu <owen.mn.siu@fwd.com>
2022-01-03 20:26:16 +0800

    remove trace input for error log

--- src/nestjs-winston-logger.service.ts
+++ src/nestjs-winston-logger.service.ts
@@ -28,8 +28,8 @@ export class NestjsWinstonLoggerService extends ConsoleLogger {
   log(message: string) {
     this.logger.info(message);
   }
-  error(message: string, trace: string) {
-    this.logger.error(message, trace);
+  error(message: string) {
+    this.logger.error(message);
   }
   warn(message: string) {
     this.logger.warn(message);
