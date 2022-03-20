d588b844e837d23f5dd9311c65bb6c7aca129b77
Layton Whiteley <layton.whiteley@gmail.com>
2022-03-20 14:31:49 +0100

    chore: reduce morgan factory to two params

--- src/morgan/morgan.middleware.ts
+++ src/morgan/morgan.middleware.ts
@@ -3,17 +3,36 @@ import { MORGAN_FORMAT_STRING } from "./morgan.constants";
 import { NestjsWinstonLoggerService } from "../nestjs-winston-logger.service";
 
 interface MorganOptions {
-  skip?: (request: Request, response: Response) => boolean;
-  immediate?: boolean;
+  skip?: (request?: Request, response?: Response) => boolean;
+  format?: string;
 }
+
+const normalizeOptions = (
+  options: string | MorganOptions,
+  defaultFormat: string,
+): MorganOptions => {
+  if (!options) {
+    return { format: defaultFormat };
+  }
+
+  if (typeof options === "string") {
+    return { format: options };
+  }
+
+  return {
+    ...options,
+    format: options.format ?? defaultFormat,
+  };
+};
 
 export const morganRequestLogger = (
   logger: NestjsWinstonLoggerService,
-  morganFormatString: string = MORGAN_FORMAT_STRING.REQUEST,
-  options: MorganOptions = {},
-) =>
-  morgan(morganFormatString, {
-    immediate: options?.immediate ?? true,
+  morganOptions: string | MorganOptions = MORGAN_FORMAT_STRING.REQUEST,
+) => {
+  const options = normalizeOptions(morganOptions, MORGAN_FORMAT_STRING.REQUEST);
+
+  return morgan(options.format, {
+    immediate: true,
     skip: options?.skip,
     stream: {
       write: (message: string) => {
@@ -21,13 +40,18 @@ export const morganRequestLogger = (
       },
     },
   });
+};
 
 export const morganResponseLogger = (
   logger: NestjsWinstonLoggerService,
-  morganFormatString: string = MORGAN_FORMAT_STRING.RESPONSE,
-  options: MorganOptions = {},
-) =>
-  morgan(morganFormatString, {
+  morganOptions: string | MorganOptions = MORGAN_FORMAT_STRING.RESPONSE,
+) => {
+  const options = normalizeOptions(
+    morganOptions,
+    MORGAN_FORMAT_STRING.RESPONSE,
+  );
+
+  return morgan(options.format, {
     skip: options?.skip,
     stream: {
       write: (message: string) => {
@@ -35,3 +59,4 @@ export const morganResponseLogger = (
       },
     },
   });
+};
